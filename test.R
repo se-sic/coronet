31953ab0f12d4b874a1ab9df5bb7e02ee7a00c2a
Claus Hunsen <hunsen@fim.uni-passau.de>
2016-10-11 10:56:08 +0200

    Update test.R
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- test.R
+++ test.R
@@ -22,13 +22,22 @@ AUTHOR.RELATION = "mail" # mail, cochange
 ARTIFACT.RELATION = "cochange" # cochange, callgraph
 
 
+## CONFIGURATION
+
 conf = CodefaceConf$new(CF.DATA, CF.SELECTION.PROCESS, CASESTUDY, ARTIFACT)
 
+## get ranges
 ranges = conf$get.ranges()
 revisions.callgraph = conf$get.revisions.callgraph()
 
-# ## project level
-# x = CodefaceProjectData$new(conf)
+
+## PROJECT-LEVEL DATA
+
+x = CodefaceProjectData$new(conf)
+# x$get.commits.raw()
+# x$get.synchronicity(time.window = c(5))
+# x$get.author2artifact()
+# x$get.commits.raw(synchronicity = TRUE, synchronicity.window = 5)
 # x$get.commits()
 # x$get.mails()
 # x$get.authors()
@@ -42,54 +51,71 @@ revisions.callgraph = conf$get.revisions.callgraph()
 # x$get.author.network(relation = AUTHOR.RELATION, directed = FALSE)
 # x$get.artifact.network(relation = ARTIFACT.RELATION, extra.edge.attr = c("hash"))
 # x$get.networks(author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION)
-# g = x$get.bipartite.network(author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION, artifact.filter.base = FALSE, author.only.committers = TRUE,
-# author.directed = FALSE)
+# g = x$get.bipartite.network(author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION, artifact.filter.base = FALSE,
+#                             author.only.committers = TRUE, author.directed = FALSE)
 # plot.bipartite.network(g)
 
-## range level
-y <- CodefaceRangeData$new(conf = conf, range = ranges[[1]], revision.callgraph = revisions.callgraph[[2]])
+# ## save binary objects
+# net = x$get.author.network(relation = "mail", directed = TRUE, simple.network = FALSE)
+# save(net, file = sprintf("busybox_%s.network", AUTHOR.RELATION))
+
+# ## example for extensive configuration
+# net = x$get.bipartite.network(author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION,
+#                               author.directed = TRUE, author.only.committers = FALSE,
+#                               artifact.extra.edge.attr = c("date", "hash"), artifact.filter.empty = TRUE,
+#                               artifact.filter = TRUE, artifact.filter.base = TRUE,
+#                               simple.network = FALSE, contract.edges = FALSE)
+# save(net, file = sprintf("busybox_%s_%s_%s.network", AUTHOR.RELATION, ARTIFACT, ARTIFACT.RELATION))
+
+
+## RANGE-LEVEL DATA
+
+y <- CodefaceRangeData$new(conf = conf, range = ranges[[2]])
 # y <- CodefaceRangeData$new(conf = conf, range = ranges[[2]], revision.callgraph = revisions.callgraph[[3]])
-# print(y$get.commits())
+# y$get.commits()
 # y$get.mails()
-# y$get.author.network(relation = AUTHOR.RELATION, directed = FALSE)
 # y$get.authors()
 # y$get.data.path()
 # y$get.data.path.range.callgraph()
-y$get.author2artifact()
+# y$get.author2artifact()
 # y$get.author2file()
 # y$get.commit2artifact(extra.data = c("file"))
 # y$get.commit2file()
 # y$get.thread2author(extra.data = "date")
-# y$get.author.network(relation = AUTHOR.RELATION, directed = TRUE, simple.network = FALSE)
-# y$get.author.network(relation = AUTHOR.RELATION, directed = FALSE)
+# y$get.author.network(relation = AUTHOR.RELATION, directed = FALSE, simple.network = FALSE)
 # y$get.artifact.network(relation = ARTIFACT.RELATION, extra.edge.attr = c("hash"))
 # y$get.networks(author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION)
-# g = y$get.bipartite.network(author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION, artifact.filter.base = FALSE, author.only.committers = TRUE,
-#                             author.directed = TRUE)
+# g = y$get.bipartite.network(author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION, artifact.filter.base = FALSE,
+#                             author.only.committers = TRUE, author.directed = TRUE)
 # plot.bipartite.network(g)
 
-# ## bulk
+
+## BULK METHODS
+
+# ## author networks
 # auth = collect.author.networks(conf, author.relation = AUTHOR.RELATION, author.directed = TRUE, simple.network = FALSE)
 # for (net in auth) {
 #     plot.author.network(net)
 # }
 #
+# ## artifact networks
 # art = collect.artifact.networks(conf, artifact.relation = c("cochange", "callgraph"),
 #                               filter.artifact = TRUE, filter.base.artifact = TRUE, extra.edge.attr = c("hash"))
 # for (net in art) {
 #     plot.artifact.network(net)
 # }
 #
+# ## bipartite networks
 # bp = collect.bipartite.networks(conf, author.relation = AUTHOR.RELATION, artifact.relation = ARTIFACT.RELATION,
 #                              simple.network = TRUE, author.directed = TRUE, artifact.extra.edge.attr = c("hash"),
 #                              artifact.filter = TRUE, artifact.filter.base = FALSE)
 # for (net in bp) {
 #     plot.bipartite.network(net)
 # }
+#
+# ## lapply on data objects
+# data = construct.data(conf, callgraphs = TRUE)
+# run.lapply(data, "get.data.path.callgraph")
 
-# plot.bipartite.network(get.sample.network())
 
-# y$save.to.disk("y.save")
 
-data = construct.data(conf, callgraphs = TRUE)
-run.lapply(data, "get.data.path.callgraph")
