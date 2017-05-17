03fde73063eec8fd6ef7e8cb1e593861923b3960
Claus Hunsen <hunsen@fim.uni-passau.de>
2017-05-17 15:21:41 +0200

    Introduce logging to splitting functions
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- util-split.R
+++ util-split.R
@@ -26,11 +26,6 @@ requireNamespace("logging")
 #' @return the list of CodefaceRangeData objects, each referring to one time period
 split.data.time.based = function(project.data, time.period = "3 months", bins = NULL,
                                  split.basis = c("commits", "mails")) {
-
-    ## get basis for splitting process
-    split.basis = match.arg(split.basis)
-    logging::loginfo(split.basis)
-
     ## get actual raw data
     data = list(
         commits = project.data$get.commits.raw(),
@@ -38,19 +33,28 @@ split.data.time.based = function(project.data, time.period = "3 months", bins = 
     )
     split.data = names(data)
     names(split.data) = split.data
-    logging::loginfo(paste(split.data, collapse = ", "))
+
+    ## get basis for splitting process
+    split.basis = match.arg(split.basis)
 
     ## if bins are NOT given explicitly
     if (is.null(bins)) {
         ## get bins based on split.basis
         bins = split.get.bins.time.based(data[[split.basis]][["date"]], time.period)$bins
         bins.labels = head(bins, -1)
+        ## logging
+        logging::loginfo("Splitting data '%s' into time ranges of %s based on '%s' data.",
+                         project.data$get.class.name(), time.period, split.basis)
     }
     ## when bins are given explicitly
     else {
+        ## get bins based on parameter
         split.basis = NULL
         bins = strftime(bins)
         bins.labels = head(bins, -1)
+        ## logging
+        logging::loginfo("Splitting data '%s' into time ranges '%s'.",
+                         project.data$get.class.name(), paste(bins, collapse = ", "), split.basis)
     }
     bins.date = as.POSIXct(bins)
 
@@ -64,6 +68,7 @@ split.data.time.based = function(project.data, time.period = "3 months", bins = 
 
     ## split data
     data.split = parallel::mclapply(split.data, function(df.name) {
+        logging::logdebug("Splitting %s.", df.name)
         ## identify bins for data
         df = data[[df.name]]
         df.bins = findInterval(df[["date"]], bins.date, all.inside = FALSE)
@@ -75,11 +80,14 @@ split.data.time.based = function(project.data, time.period = "3 months", bins = 
     })
 
     ## re-arrange data to get the proper list of data per range
+    logging::logdebug("Re-arranging data.")
     data.split = parallel::mclapply(bins.labels, function(bin) lapply(data.split, `[[`, bin))
     names(data.split) = bins.ranges
 
     ## construct CodefaceRangeData objects
+    logging::logdebug("Constructing CodefaceRangeData objects.")
     cf.data = parallel::mclapply(bins.ranges, function(range) {
+        logging::logdebug("Constructing data for range %s.", range)
         ## construct object for current range
         cf.range.data = CodefaceRangeData$new(conf, range)
         ## FIXME add revision.callgraph parameter
@@ -116,7 +124,8 @@ split.data.activity.based = function(project.data, activity.type = c("commits", 
 
     ## get basis for splitting process
     activity.type = match.arg(activity.type)
-    logging::loginfo(activity.type)
+    logging::loginfo("Splitting data '%s' into activity ranges of %s %s.",
+                     project.data$get.class.name(), activity.amount, activity.type)
 
     ## get actual raw data
     data = list(
@@ -131,10 +140,12 @@ split.data.activity.based = function(project.data, activity.type = c("commits", 
     )
 
     ## get bins based on split.basis
+    logging::logdebug("Getting activity-based bins.")
     bins = split.get.bins.activity.based(data[[activity.type]], id.column[[activity.type]], activity.amount)[["bins"]]
     bins.date = as.POSIXct(bins)
 
     ## split the data based on the extracted timestamps
+    logging::logdebug("Splitting data based on time windows arising from activity bins.")
     data.split = split.data.time.based(project.data, bins = bins.date, split.basis = activity.type)
     return(data.split)
 }
@@ -160,10 +171,15 @@ split.network.time.based = function(network, time.period, bins = NULL) {
         bins.date = as.POSIXct(bins)
         bins.vector = findInterval(dates, bins.date, all.inside = FALSE)
         bins = 1:(length(bins.date) - 1) # the last item just closes the last bin
+        ## logging
+        logging::loginfo("Splitting network into time ranges of %s.", time.period)
     } else {
         bins.info = split.get.bins.time.based(dates, time.period)
         bins.vector = bins.info[["vector"]]
         bins = head(bins.info[["bins"]], -1)
+        ## logging
+        logging::loginfo("Splitting network '%s' into time ranges '%s'.",
+                         paste(bins.info[["bins"]], collapse = ", "))
     }
 
     nets = split.network.by.bins(network, bins, bins.vector)
@@ -196,6 +212,9 @@ split.network.activity.based = function(network, number.edges = 5000, number.win
         # compute the number of time windows according to the number of edges per network
         number.windows = ceiling(edge.count / number.edges)
 
+        logging::loginfo("Splitting network into activity ranges of %s edges, yielding %s windows.",
+                         number.edges, number.windows)
+
         ## get dates in a data.frame for splitting purposes
         df = data.frame(
             date = as.POSIXct( # use POSIXct as other functions need it
@@ -206,6 +225,7 @@ split.network.activity.based = function(network, number.edges = 5000, number.win
         df = df[ with(df, order(date)), ]
 
         ## identify bins
+        logging::logdebug("Getting bins for activity-based splitting based on amount of edges.")
         bins.vector = split.get.bins.activity.based(df, "my.unique.id", activity.amount = number.edges)[["vector"]]
         bins.vector = bins.vector[ with(df, order(my.unique.id)) ] # re-order to get igraph ordering
         bins = sort(unique(bins.vector))
@@ -220,11 +240,14 @@ split.network.activity.based = function(network, number.edges = 5000, number.win
             stop("Stopping due to illegally specified amount of windows to create.")
         }
 
+        logging::loginfo("Splitting network into %s windows.", number.windows)
+
         ## get dates from first and last edge
         date.start = as.POSIXct(min(igraph::E(network)$date), origin = "1970-01-01")
         date.end = as.POSIXct(max(igraph::E(network)$date), origin = "1970-01-01") + 1 # plus one second
 
         ## create time-based equidistant bins
+        logging::logdebug("Getting bins for activity-based splitting based on amount of windows.")
         bins = seq(date.start, date.end, length.out = number.windows + 1)
 
         ## split network with the computed bins
@@ -241,7 +264,9 @@ split.network.activity.based = function(network, number.edges = 5000, number.win
 #'
 #' @return a list of data.frames, with the length of 'unique(bins)'
 split.data.by.bins = function(df, bins) {
+    logging::logdebug("split.data.by.bins: starting.")
     df.split = split(df, bins)
+    logging::logdebug("split.data.by.bins: finished.")
     return(df.split)
 }
 
@@ -253,15 +278,17 @@ split.data.by.bins = function(df, bins) {
 #'
 #' @return a list of networks, with the length of 'unique(bins.vector)'
 split.network.by.bins = function(network, bins, bins.vector) {
+    logging::logdebug("split.data.time.based: starting.")
     ## create a network for each bin of edges
     nets = parallel::mclapply(bins, function(bin) {
+        logging::logdebug("Splitting network: bin %s", bin)
         ## identify edges in the current bin
         edges = igraph::E(network)[ bins.vector == bin ]
         ## create network based on the current set of edges
         g = igraph::subgraph.edges(network, edges, delete.vertices = TRUE)
         return(g)
     })
-
+    logging::logdebug("split.data.time.based: finished.")
     return(nets)
 }
 
@@ -281,6 +308,7 @@ split.network.by.bins = function(network, bins, bins.vector) {
 #'             each item in the vector indicates the start of a bin, although the last
 #'             item indicates the end of the last bin
 split.get.bins.time.based = function(dates, time.period) {
+    logging::logdebug("split.get.bins.time.based: starting.")
     ## find bins for given dates
     dates.bins = cut(dates, breaks = time.period)
     ## get bins for returning
@@ -294,6 +322,7 @@ split.get.bins.time.based = function(dates, time.period) {
             length = 2)[2]
         )
     )
+    logging::logdebug("split.get.bins.time.based: finished.")
     return(list(
         vector = dates.bins,
         bins = bins
@@ -313,6 +342,7 @@ split.get.bins.time.based = function(dates, time.period) {
 #'         'acitivity.amount' many unique items; each item in the vector indicates
 #'         the start of a bin, although the last item indicates the end of the last bin
 split.get.bins.activity.based = function(df, id, activity.amount) {
+    logging::logdebug("split.get.bins.activity.based: starting")
     ## get the unique integer IDs for each item in 'id' column
     ids = df[[id]]
     ids.unique = unique(ids)
@@ -358,6 +388,7 @@ split.get.bins.activity.based = function(df, id, activity.amount) {
     ## convert to character strings
     bins.date.char = strftime(bins.date)
 
+    logging::logdebug("split.get.bins.activity.based: finished.")
     return(list(
         vector = bins.activity,
         bins = bins.date.char
