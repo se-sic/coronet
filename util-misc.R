fb52357f05958007b867da06f4077abdc04fa0d8
Claus Hunsen <hunsen@fim.uni-passau.de>
2017-07-25 10:30:07 +0200

    Fix bug in 'simplify.networks' function (#48)
    Fixes #48.
    
    Props to @bockthom for spotting this.
    
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- util-misc.R
+++ util-misc.R
@@ -355,6 +355,7 @@ simplify.networks = function(networks){
     nets = parallel::mclapply(networks, simplify.network)
 
     logging::logdebug("simplify.networks: finished.")
+    return(nets)
 }
 
 
