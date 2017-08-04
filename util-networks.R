e490ed06b98aba4226f672e1b96a1e2438d8dfc3
Claus Hunsen <hunsen@fim.uni-passau.de>
2017-08-04 17:06:45 +0200

    Add edge weights to sample network
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- util-networks.R
+++ util-networks.R
@@ -434,11 +434,11 @@ get.sample.network = function(network.conf = NetworkConf$new()) {
     ## INDEPENDENT NETWORKS
     authors = igraph::graph.empty(directed = FALSE) +
         igraph::vertices("D1", "D2", "D3", "D4", "D5", "D6", type = TYPE.AUTHOR) +
-        igraph::edges("D1", "D2", "D1", "D4", "D3", "D4", "D4", "D5", type = TYPE.EDGES.INTRA)
+        igraph::edges("D1", "D2", "D1", "D4", "D3", "D4", "D4", "D5", type = TYPE.EDGES.INTRA, weight = 1)
 
     artifacts = igraph::graph.empty(directed = FALSE) +
         igraph::vertices("A1", "A2", "A3", "A4", "A5", "A6", type = TYPE.ARTIFACT) +
-        igraph::edges("A1", "A2", "A1", "A3", "A2", "A3", "A2", "A4", "A5", "A6", type = TYPE.EDGES.INTRA)
+        igraph::edges("A1", "A2", "A1", "A3", "A2", "A3", "A2", "A4", "A5", "A6", type = TYPE.EDGES.INTRA, weight = 1)
     # artifacts = igraph::as.directed(artifacts, mode = "mutual")
 
     authors.to.artifacts.df = data.frame(
