8625272d3b0fddfea482ce8da2fd44afb728c5cc
Felix Prasse <prassefe@fim.uni-passau.de>
2017-11-28 23:08:20 +0100

    Fix issue with ranges missing time
    Signed-off-by: Felix Prasse <prassefe@fim.uni-passau.de>

--- util-misc.R
+++ util-misc.R
@@ -100,7 +100,8 @@ get.range.bounds = function(range) {
     ## the patterns to test with appropriate conversions (if any)
     tests = list(
         ## date format (assuming dates are GMT)
-        c("\\d{4}-\\d{2}-\\d{2}(\\s\\d{2}:\\d{2}:\\d{2})?", as.POSIXct),
+        ## Add zeros for dates without time. Will be ignored if there is a time already.
+        c("\\d{4}-\\d{2}-\\d{2}(\\s\\d{2}:\\d{2}:\\d{2})?", function(x) as.POSIXct(paste(x, " 00:00:00"))),
 
         ## commit format
         c("[A-F0-9a-f]{40}", identity),
