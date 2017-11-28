046b5e588aec56b2765fd11e0c7ba2161d6931c0
Felix Prasse <prassefe@fim.uni-passau.de>
2017-11-28 11:28:00 +0100

    Add test for get.range.bounds
    Signed-off-by: Felix Prasse <prassefe@fim.uni-passau.de>

---
+++ tests/test-misc.R
@@ -0,0 +1,18 @@
+## (c) Felix Prasse, 2017
+## prassefe@fim.uni-passau.de
+
+test_that("Parse range", {
+    range.input = c("2012-07-10 15:58:00-2012-07-15 16:02:00",
+                    "f86391e7d7eaf4234fc742c1b61f32cb8a65782e-63654c1b089b8abe9a52d21fd1b53b1631539e10",
+                    "v1.0.0-v2.0.0_alpha")
+
+    expected.output = list(
+        c(as.POSIXct("2012-07-10 15:58:00"), as.POSIXct("2012-07-15 16:02:00")),
+        c("f86391e7d7eaf4234fc742c1b61f32cb8a65782e", "63654c1b089b8abe9a52d21fd1b53b1631539e10"),
+        c("v1.0.0", "v2.0.0_alpha")
+    )
+
+    actual.output = lapply(range.input, get.range.bounds)
+
+    expect_equal(actual.output, expected.output, "Parsed Range")
+})
