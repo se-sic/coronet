e3c65102ff0030b951a594bf407ad6773b321713
Felix Prasse <prassefe@fim.uni-passau.de>
2017-12-04 18:24:59 +0100

    Rename what parameter to aggregation
    Signed-off-by: Felix Prasse <prassefe@fim.uni-passau.de>

--- util-networks-covariates.R
+++ util-networks-covariates.R
@@ -8,12 +8,12 @@
 #' @param list.of.networks The list of networks to add vertex attributes to
 #' @param project.data The entire project date
 #' @param attr.name The name of the attribute to add
-#' @param what One of range, cumulative or all. Determines the data to use for the attribute calculation.
+#' @param aggretation One of range, cumulative or all. Determines the data to use for the attribute calculation.
 #' @param compute.attr The function to compute the attribute to add. Must return a named list
 #'                  with the names being the name of the vertex.
 #'
-compute.vertex.attributes = function(list.of.networks, project.data, attr.name, what = c("range", "cumulative", "all"), compute.attr) {
-    net.to.range.list = networks.to.ranges(list.of.networks, project.data, what)
+compute.vertex.attributes = function(list.of.networks, project.data, attr.name, aggretation = c("range", "cumulative", "all"), compute.attr) {
+    net.to.range.list = networks.to.ranges(list.of.networks, project.data, aggretation)
 
     return (compute.vertex.attributes.with.n2r(net.to.range.list, attr.name, compute.attr))
 }
@@ -31,19 +31,19 @@ compute.vertex.attributes.with.n2r = function(net.to.range.list, attr.name, comp
     }))
 }
 
-networks.to.ranges = function(list.of.networks, project.data, what = c("range", "cumulative", "all")) {
+networks.to.ranges = function(list.of.networks, project.data, aggretation = c("range", "cumulative", "all")) {
     list.of.ranges = as.list(names(list.of.networks))
 
-    what = match.arg(what)
+    aggretation = match.arg(aggretation)
 
     net.to.range.list = lapply(list.of.ranges, function(range) {
         start.end = get.range.bounds(range)
 
-        if(what == "cumulative" || what == "all") {
+        if(aggretation == "cumulative" || aggretation == "all") {
             start.end[1] = as.POSIXct("1970-01-01 00:00:00")
         }
 
-        if(what == "all") {
+        if(aggretation == "all") {
             start.end[2] = as.POSIXct("9999-01-01 00:00:00")
         }
 
@@ -57,8 +57,8 @@ networks.to.ranges = function(list.of.networks, project.data, what = c("range", 
     return(net.to.range.list)
 }
 
-add.vertex.attributes.commit.count = function(list.of.networks, data, name = "commit.count", what = c("range", "cumulative", "all")) {
-    compute.vertex.attributes(list.of.networks, data, name, what, function(range.data, net) {
+add.vertex.attributes.commit.count = function(list.of.networks, data, name = "commit.count", aggretation = c("range", "cumulative", "all")) {
+    compute.vertex.attributes(list.of.networks, data, name, aggretation, function(range.data, net) {
         commit.count.df = get.author.commit.count(range.data)[c("author.name", "freq")]
         commit.count.list = structure(commit.count.df["freq"], names = commit.count.df["author.name"])
 
@@ -68,27 +68,25 @@ add.vertex.attributes.commit.count = function(list.of.networks, data, name = "co
 
 
 
-add.vertex.attributes.author.email = function(list.of.networks, data, name = "author.email", what = c("range", "cumulative", "all")) {
-    compute.vertex.attributes(list.of.networks, data, name, what,
+add.vertex.attributes.author.email = function(list.of.networks, data, name = "author.email", aggretation = c("range", "cumulative", "all")) {
+    compute.vertex.attributes(list.of.networks, data, name, aggretation,
                               function(range.data, net) {
-                                  author.to.mail = range.data$get.author2commit()
-                                  author.to.mail = lapply(author.to.mail, function(a) {
-                                      return ( unique(a["author.email"]) )
-                                      })
+                                  authors = range.data$get.authors()
 
-                                  return (author.to.mail)
+                                  return (structure(names = authors[["author.name"]],
+                                                    authors[["author.email"]]))
                               })
 }
 
-add.vertex.attributes.artifact.count = function(list.of.networks, data, what = c("range", "cumulative", "all")) {
-    compute.vertex.attributes(list.of.networks, data, "artifact.count", what,
+add.vertex.attributes.artifact.count = function(list.of.networks, data, aggretation = c("range", "cumulative", "all")) {
+    compute.vertex.attributes(list.of.networks, data, "artifact.count", aggretation,
                               function(range.data, net) {
                                   return (lapply(range.data$get.author2artifact(), function(x) length(unique(x))))
                               })
 }
 
-add.vertex.attributes.first.activity = function(list.of.networks, data, what = c("range", "cumulative", "all")) {
-    compute.vertex.attributes(list.of.networks, data, "first.activity", what,
+add.vertex.attributes.first.activity = function(list.of.networks, data, aggretation = c("range", "cumulative", "all")) {
+    compute.vertex.attributes(list.of.networks, data, "first.activity", aggretation,
                               function(range.data, net) {
                                   return (lapply(range.data$get.author2commit(),
                                          function(x) { return ( min(x["date"]) ) }))
@@ -116,9 +114,9 @@ add.vertex.attributes.active.ranges = function(list.of.networks, data) {
                               function(range.data, net) active.ranges)
 }
 
-add.vertex.attributes.author.role = function(list.of.networks, data, what = c("range", "cumulative", "all"),
+add.vertex.attributes.author.role = function(list.of.networks, data, aggretation = c("range", "cumulative", "all"),
                                      type = c("network.degree", "network.eigen", "commit.count", "loc.count")) {
-    compute.vertex.attributes(list.of.networks, data, "classification", what,
+    compute.vertex.attributes(list.of.networks, data, "classification", aggretation,
                               function(range.data, net) {
                                   author.class = get.author.class.by.type(net, range.data, type)
                                   author.class = plyr::ldply(author.class, .id = "type")
