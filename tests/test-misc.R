87829a7f9eafe7f8e8cda87db80ccb25a06c78bd
Claus Hunsen <hunsen@fim.uni-passau.de>
2018-02-21 16:26:59 +0100

    Add tests for cumulative and aggregated ranges
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- tests/test-misc.R
+++ tests/test-misc.R
@@ -72,6 +72,113 @@ test_that("Construct consecutive and overlapping ranges.", {
 })
 
 ##
+## Construct cumulative ranges.
+##
+
+test_that("Construct cumulative ranges.", {
+
+    start = ("2018-01-01 00:00:00")
+    start.date = get.date.from.string(start)
+    end = ("2018-01-01 06:05:00")
+    end.date = get.date.from.string(end)
+    end.including = end.date + 1
+
+    ## standard overlapping ranges:
+    ## 1) expected results
+    expected.formatted = c(
+        "2018-01-01 00:00:00-2018-01-01 02:00:00",
+        "2018-01-01 00:00:00-2018-01-01 04:00:00",
+        "2018-01-01 00:00:00-2018-01-01 06:00:00",
+        "2018-01-01 00:00:00-2018-01-01 06:05:01"
+    )
+    expected.raw = lapply(expected.formatted, get.range.bounds)
+    names(expected.raw) = expected.formatted
+    ## 2) formatted
+    result.formatted = construct.cumulative.ranges(start, end, time.period = "2 hours", raw = FALSE)
+    expect_identical(result.formatted, expected.formatted, info = "Cumulative ranges (formatted).")
+    ## 3) raw
+    result.raw = construct.cumulative.ranges(start, end, time.period = "2 hours", raw = TRUE)
+    expect_equal(result.raw, expected.raw, info = "Cumulative ranges (raw).")
+    ## TODO use expect_identical here? why failing?
+})
+
+##
+## Aggregate ranges.
+##
+
+test_that("Aggregate ranges.", {
+
+    aggregation.level = c("range", "cumulative", "all.ranges",
+                          "project.cumulative", "project.all.ranges",
+                          "complete")
+
+    project.start = get.date.from.string("2017-12-01 00:00:00")
+    start = ("2018-01-01 00:00:00")
+    start.date = get.date.from.string(start)
+    end = ("2018-01-01 06:05:00")
+    end.date = get.date.from.string(end)
+    end.including = end.date + 1
+    project.end = get.date.from.string("2019-04-04 00:00:00")
+
+    ## construct ranges to aggregate
+    ranges = construct.consecutive.ranges(start.date, end.date, time.period = "2 hours", raw = FALSE)
+    ranges.raw =  construct.consecutive.ranges(start.date, end.date, time.period = "2 hours", raw = FALSE)
+
+    ## get results
+    results = lapply(aggregation.level, function(level)
+        aggregate.ranges(ranges, project.start, project.end, level, raw = FALSE) ## FIXME raw = TRUE
+    )
+    names(results) = aggregation.level
+
+    ## expected results
+    expected = list(
+        range = c(
+            "2018-01-01 00:00:00-2018-01-01 02:00:00",
+            "2018-01-01 02:00:00-2018-01-01 04:00:00",
+            "2018-01-01 04:00:00-2018-01-01 06:00:00",
+            "2018-01-01 06:00:00-2018-01-01 06:05:01"
+        ),
+        cumulative = c(
+            "2018-01-01 00:00:00-2018-01-01 02:00:00",
+            "2018-01-01 00:00:00-2018-01-01 04:00:00",
+            "2018-01-01 00:00:00-2018-01-01 06:00:00",
+            "2018-01-01 00:00:00-2018-01-01 06:05:01"
+        ),
+        all.ranges = c(
+            "2018-01-01 00:00:00-2018-01-01 06:05:01",
+            "2018-01-01 00:00:00-2018-01-01 06:05:01",
+            "2018-01-01 00:00:00-2018-01-01 06:05:01",
+            "2018-01-01 00:00:00-2018-01-01 06:05:01"
+        ),
+        project.cumulative = c(
+            "2017-12-01 00:00:00-2018-01-01 02:00:00",
+            "2017-12-01 00:00:00-2018-01-01 04:00:00",
+            "2017-12-01 00:00:00-2018-01-01 06:00:00",
+            "2017-12-01 00:00:00-2018-01-01 06:05:01"
+        ),
+        project.all.ranges = c(
+            "2017-12-01 00:00:00-2018-01-01 06:05:01",
+            "2017-12-01 00:00:00-2018-01-01 06:05:01",
+            "2017-12-01 00:00:00-2018-01-01 06:05:01",
+            "2017-12-01 00:00:00-2018-01-01 06:05:01"
+        ),
+        complete = c(
+            "2017-12-01 00:00:00-2019-04-04 00:00:01",
+            "2017-12-01 00:00:00-2019-04-04 00:00:01",
+            "2017-12-01 00:00:00-2019-04-04 00:00:01",
+            "2017-12-01 00:00:00-2019-04-04 00:00:01"
+        )
+    )
+
+    lapply(aggregation.level, function(aggregation.level) {
+        expected.ranges = expected[[aggregation.level]]
+        results.ranges = results[[aggregation.level]]
+
+        expect_identical(results.ranges, expected.ranges)
+    })
+})
+
+##
 ## Parse ranges.
 ##
 
