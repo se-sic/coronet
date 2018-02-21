db0cb14a0222bfb069a758ed45db16232db225f9
Claus Hunsen <hunsen@fim.uni-passau.de>
2018-02-21 15:53:50 +0100

    Add test for consecutive and overlapping ranges
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- tests/test-misc.R
+++ tests/test-misc.R
@@ -5,7 +5,78 @@
 ## (c) Thomas Bock, 2017
 ## bockthom@fim.uni-passau.de
 
+
+## / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+## Date handling -----------------------------------------------------------
+
+
+## / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
+## Range construction and handling -----------------------------------------
+
+##
+## Construct consecutive and overlapping ranges.
+##
+
+test_that("Construct consecutive and overlapping ranges.", {
+
+    start = ("2018-01-01 00:00:00")
+    start.date = get.date.from.string(start)
+    end = ("2018-01-01 06:05:00")
+    end.date = get.date.from.string(end)
+    end.including = end.date + 1
+
+    ## standard overlapping ranges:
+    ## 1) expected results
+    expected.formatted = c(
+        "2018-01-01 00:00:00-2018-01-01 02:00:00",
+        "2018-01-01 01:30:00-2018-01-01 03:30:00",
+        "2018-01-01 03:00:00-2018-01-01 05:00:00",
+        "2018-01-01 04:30:00-2018-01-01 06:05:01"
+    )
+    expected.raw = lapply(expected.formatted, get.range.bounds)
+    names(expected.raw) = expected.formatted
+    ## 2) formatted
+    result.formatted = construct.overlapping.ranges(start, end, time.period = "2 hours", overlap = "30 minutes", raw = FALSE)
+    expect_identical(result.formatted, expected.formatted, info = "Standard overlapping ranges (formatted).")
+    ## 3) raw
+    result.raw = construct.overlapping.ranges(start, end, time.period = "2 hours", overlap = "30 minutes", raw = TRUE)
+    expect_equal(result.raw, expected.raw, info = "Standard overlapping ranges (raw).")
+    ## TODO use expect_identical here? why failing?
+
+    ## non-overlapping/consecutive ranges:
+    ## 1) expected results
+    expected.formatted = c(
+        "2018-01-01 00:00:00-2018-01-01 02:00:00",
+        "2018-01-01 02:00:00-2018-01-01 04:00:00",
+        "2018-01-01 04:00:00-2018-01-01 06:00:00",
+        "2018-01-01 06:00:00-2018-01-01 06:05:01"
+    )
+    expected.raw = lapply(expected.formatted, get.range.bounds)
+    names(expected.raw) = expected.formatted
+    ## 2) formatted
+    result.formatted = construct.overlapping.ranges(start.date, end.date, time.period = "2 hours", overlap = 0, raw = FALSE)
+    expect_identical(result.formatted, expected.formatted, info = "Non-overlapping ranges (formatted).")
+    ## 3) raw
+    result.raw = construct.overlapping.ranges(start.date, end.date, time.period = "2 hours", overlap = 0, raw = TRUE)
+    expect_equal(result.raw, expected.raw, info = "Non-overlapping ranges (raw).")
+    ## TODO use expect_identical here? why failing?
+    ## 4) matching with consecutive ranges
+    results.raw = construct.consecutive.ranges(start.date, end.date, time.period = "2 hours", raw = FALSE)
+    expect_equal(result.raw, expected.raw, info = "Non-overlapping ranges (consecutive).")
+
+    ## illegal overlap
+    expect_error(
+        construct.overlapping.ranges(start.date, end.date, time.period = "2 hours", overlap = "1 year", raw = FALSE),
+        info = "Error expected (illegal overlap)."
+    )
+})
+
+##
+## Parse ranges.
+##
+
 test_that("Parse range", {
+
     range.input = c("2012-07-10 15:58:00-2012-07-15 16:02:00",
                     "2012-07-10-2012-07-15 16:02:00",
                     "2012-07-10 15:58:00-2012-07-15",
@@ -24,3 +95,4 @@ test_that("Parse range", {
 
     expect_equal(actual.output, expected.output, "Parsed Range")
 })
+
