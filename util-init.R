a14d750df64ac4cf407342cf2a1878d338f83235
Claus Hunsen <hunsen@fim.uni-passau.de>
2018-06-06 16:13:52 +0200

    Adjust copyright header in initialization module
    Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

--- util-init.R
+++ util-init.R
@@ -11,7 +11,7 @@
 ## with this program; if not, write to the Free Software Foundation, Inc.,
 ## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ##
-## Copyright 2016-2017 by Claus Hunsen <hunsen@fim.uni-passau.de>
+## Copyright 2016-2018 by Claus Hunsen <hunsen@fim.uni-passau.de>
 ## Copyright 2017 by Christian Hechtl <hechtl@fim.uni-passau.de>
 ## Copyright 2017 by Raphael Nömmer <noemmer@fim.uni-passau.de>
 ## Copyright 2017 by Sofie Kemper <kemperso@fim.uni-passau.de>
