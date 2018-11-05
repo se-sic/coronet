se-sic/coronet

commit 1bed431bf21d78936391a5e7278de5f762922eac

Disable caching of artifacts in ProjectData objects
To circumvent problems when updating the raw commits but not updating
the cached list of artifacts, we disable the caching of artifacts for
now.

Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>
Signed-off-by: Thomas Bock <bockthom@fim.uni-passau.de>

util-data.R
@@ -59,13 +59,12 @@ ProjectData = R6::R6Class("ProjectData",
         commits.filtered = NULL, # data.frame
         commits.filtered.empty = NULL, #data.frame
         commits = NULL, # data.frame
-        artifacts = NULL, # list
         synchronicity = NULL, # data.frame
         pasta = NULL, # data.frame
         ## mails
         mails = NULL, # data.frame
         ## authors
-        authors = NULL, # list
+        authors = NULL, # data.frame
         ## issues
         issues = NULL, #data.frame
         ## timestamps of mail, issue and commit data
@@ -584,22 +583,17 @@ ProjectData = R6::R6Class("ProjectData",
         #'
         #' @return the list of artifacts
         get.artifacts = function() {
-            ## FIXME the artifacts determination should be dependent on the artifact.relation
+            ## FIXME the artifacts determination should be dependent on the artifact.relation (which is in the net.conf which itself is not available here!)
             ## (see also get.author2artifact)
             logging::loginfo("Getting artifact data.")
 
-            ## if artifacts are not read already, do this
-            if (is.null(private$artifacts)) {
-                commits = self$get.commits.filtered.empty()
+            commits = self$get.commits.filtered.empty()
 
-                ## get artifacts (empty list if no commits exist)
-                artifacts = unique(commits[["artifact"]])
-                if (is.null(artifacts)) artifacts = list()
+            ## get artifacts (empty list if no commits exist)
+            artifacts = unique(commits[["artifact"]])
+            if (is.null(artifacts)) artifacts = list()
 
-                private$artifacts = artifacts
-            }
-
-            return(private$artifacts)
+            return(artifacts)
         },
 
         #' Get single pasta items.

commit c64cab84e928a2a4c89a6df12440ba7ca06e6263

Fix error when resetting an ProjectData environment
When calling the method 'ProjectData$reset.environment()', an error is
produced:
> Error in private$artifacts = NULL :
>   cannot add bindings to a locked environment

This is due a regression introduced in commit
1bed431bf21d78936391a5e7278de5f762922eac, where the field
'ProjectData$artifacts' has been removed, but not the corresponding
statement for resetting it. This is fixed now.

Signed-off-by: Claus Hunsen <hunsen@fim.uni-passau.de>

util-data.R
@@ -289,7 +289,6 @@ ProjectData = R6::R6Class("ProjectData",
             private$commits.filtered = NULL
             private$commits.filtered.empty = NULL
             private$commits = NULL
-            private$artifacts = NULL
             private$synchronicity = NULL
             private$mails = NULL
             private$authors = NULL