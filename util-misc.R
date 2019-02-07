27d308506c23cd7ec7b9006041644a2f42f8adcd
Jakob Kronawitter <kronawij@fim.uni-passau.de>
2019-02-07 20:53:25 +0100

    Update copyright headers
    Signed-off-by: Jakob Kronawitter <kronawij@fim.uni-passau.de>

--- util-misc.R
+++ util-misc.R
@@ -16,7 +16,7 @@
 ## Copyright 2017 by Christian Hechtl <hechtl@fim.uni-passau.de>
 ## Copyright 2017 by Felix Prasse <prassefe@fim.uni-passau.de>
 ## Copyright 2017-2018 by Thomas Bock <bockthom@fim.uni-passau.de>
-## Copyright 2018 by Jakob Kronawitter <kronawij@fim.uni-passau.de>
+## Copyright 2018-2019 by Jakob Kronawitter <kronawij@fim.uni-passau.de>
 ## All Rights Reserved.
 
 
