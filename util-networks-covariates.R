cd22b4756d7f5b6b5055af978cc13f78e01c663e
Klara <schluete@fim.uni-passau.de>
2019-03-20 14:42:30 +0100

    Update copyright headers
    Signed-off-by: Klara Schlueter <schluete@fim.uni-passau.de>

--- util-networks-covariates.R
+++ util-networks-covariates.R
@@ -14,7 +14,7 @@
 ## Copyright 2017 by Felix Prasse <prassefe@fim.uni-passau.de>
 ## Copyright 2018 by Claus Hunsen <hunsen@fim.uni-passau.de>
 ## Copyright 2018 by Thomas Bock <bockthom@fim.uni-passau.de>
-## Copyright 2018 by Klara Schlüter <schluete@fim.uni-passau.de>
+## Copyright 2018-2019 by Klara Schlüter <schluete@fim.uni-passau.de>
 ## Copyright 2018 by Jakob Kronawitter <kronawij@fim.uni-passau.de>
 ## All Rights Reserved.
 
