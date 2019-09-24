f014eeb182f07df106c603fc8470067020fe701c
Jakob <kronawij@fim.uni-passau.de>
2019-09-24 11:50:47 +0200

    Update copyright headers
    Signed-off-by: Jakob Kronawitter <kronawij@fim.uni-passau.de>

--- tests/test-data.R
+++ tests/test-data.R
@@ -12,7 +12,8 @@
 ## 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ##
 ## Copyright 2018 by Christian Hechtl <hechtl@fim.uni-passau.de>
-## Copyright 2018 by Claus Hunsen <hunsen@fim.uni-passau.de>
+## Copyright 2018-2019 by Claus Hunsen <hunsen@fim.uni-passau.de>
+## Copyright 2019 by Jakob Kronawitter <kronawij@fim.uni-passau.de>
 ## All Rights Reserved.
 
 
