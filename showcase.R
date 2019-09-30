b0754f53ec694de18ba44834591be1db3c1529b5
Klara <schluete@fim.uni-passau.de>
2019-09-30 14:08:56 +0200

    Remove wrong copyright header from showcase.R
    Signed-off-by: Klara Schlueter <schluete@fim.uni-passau.de>

--- showcase.R
+++ showcase.R
@@ -17,7 +17,6 @@
 ## Copyright 2017 by Felix Prasse <prassefe@fim.uni-passau.de>
 ## Copyright 2017-2018 by Thomas Bock <bockthom@fim.uni-passau.de>
 ## Copyright 2018 by Jakob Kronawitter <kronawij@fim.uni-passau.de>
-## Copyright 2019 by Klara Schlüter <schluete@fim.uni-passau.de>
 ## All Rights Reserved.
 
 
