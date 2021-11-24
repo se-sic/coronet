53ef8cdde8c83e45c9a9379f88e9a22b9d8d7e01
Mirabdulla Yusifli <miriyusifli@Miri-PC>
2021-11-24 07:29:52 +0100

    Place gender section below author section
    Move functions related to gender data to the 'additional data sources'
    section to keep a meaningful order of the sections.
    
    Signed-off-by: Mirabdulla Yusifli <s8miyusi@stud.uni-saarland.de>

--- util-read.R
+++ util-read.R
@@ -388,81 +388,6 @@ read.issues = function(data.path, issues.sources = c("jira", "github")) {
 }
 
 
-## * Gender data ------------------------------------------------------------
-
-## column names of a dataframe containing gender data (see function \code{read.gender})
-GENDER.LIST.COLUMNS = c(
-    "author.name", "gender"
-)
-
-## declare the datatype for each column in the constant 'GENDER.LIST.COLUMNS'
-GENDER.LIST.DATA.TYPES = c(
-    "character", "character"
-)
-
-#' Read and parse the gender data from the 'gender' file.
-#' The form in the file is : author.name, gender
-#' The parsed form is a data frame with author.name as key, gender as value.
-#'
-#' @param data.path the path to the gender data
-#'
-#' @return the read and parsed gender data
-read.gender = function(data.path) {
-    # constant for seperating key and value
-    SEPERATOR = ";"
-
-    ## get file name of gender data
-    filepath = file.path(data.path, "gender")
-
-    ## read data from disk [can be empty]
-    lines = suppressWarnings(try(readLines(filepath), silent = TRUE))
-
-    ## handle the case if the list of items is empty
-    if (inherits(lines, "try-error")) {
-        logging::logwarn("There are no gender data available for the current environment.")
-        logging::logwarn("Datapath: %s", data.path)
-        return(create.empty.gender.list())
-    }
-
-    result.list = parallel::mcmapply(lines, seq_along(lines), SIMPLIFY = FALSE, FUN = function(line, line.id) {
-        if ( nchar(line) == 0 ) {
-            return(NULL)
-        }
-
-        # 1) split key
-        # 2) split value
-        line.split = unlist(strsplit(line, SEPERATOR))
-        key = line.split[1]
-        value = line.split[2]
-
-        # Transform data to data.frame
-        df = merge(key, value)
-        colnames(df) = c("author.name", "gender")
-        return(df)
-    })
-
-    result.df = plyr::rbind.fill(result.list)
-    logging::logdebug("read.gender: finished.")
-    return(result.df)
-}
-
-#' Create an empty dataframe which has the same shape as a dataframe containing issues. The dataframe has the column
-#' names and column datatypes defined in \code{ISSUES.LIST.COLUMNS} and \code{ISSUES.LIST.DATA.TYPES}, respectively.
-#'
-#' @return the empty dataframe
-create.empty.issues.list = function() {
-    return (create.empty.data.frame(ISSUES.LIST.COLUMNS, ISSUES.LIST.DATA.TYPES))
-}
-
-#' Create an empty dataframe which has the same shape as a dataframe containing gender data.
-#' The dataframe has the column names and column datatypes defined in \code{GENDER.LIST.COLUMNS}
-#' and \code{GENDER.LIST.DATA.TYPES}, respectively.
-#'
-#' @return the empty dataframe
-create.empty.gender.list = function() {
-    return (create.empty.data.frame(GENDER.LIST.COLUMNS, GENDER.LIST.DATA.TYPES))
-}
-
 ## / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / / /
 ## Additional data sources -------------------------------------------------
 
@@ -577,6 +502,83 @@ read.authors = function(data.path) {
 create.empty.authors.list = function() {
     return (create.empty.data.frame(AUTHORS.LIST.COLUMNS, AUTHORS.LIST.DATA.TYPES))
 }
+
+
+## * Gender data ------------------------------------------------------------
+
+## column names of a dataframe containing gender data (see function \code{read.gender})
+GENDER.LIST.COLUMNS = c(
+    "author.name", "gender"
+)
+
+## declare the datatype for each column in the constant 'GENDER.LIST.COLUMNS'
+GENDER.LIST.DATA.TYPES = c(
+    "character", "character"
+)
+
+#' Read and parse the gender data from the 'gender' file.
+#' The form in the file is : author.name, gender
+#' The parsed form is a data frame with author.name as key, gender as value.
+#'
+#' @param data.path the path to the gender data
+#'
+#' @return the read and parsed gender data
+read.gender = function(data.path) {
+    # constant for seperating key and value
+    SEPERATOR = ";"
+
+    ## get file name of gender data
+    filepath = file.path(data.path, "gender")
+
+    ## read data from disk [can be empty]
+    lines = suppressWarnings(try(readLines(filepath), silent = TRUE))
+
+    ## handle the case if the list of items is empty
+    if (inherits(lines, "try-error")) {
+        logging::logwarn("There are no gender data available for the current environment.")
+        logging::logwarn("Datapath: %s", data.path)
+        return(create.empty.gender.list())
+    }
+
+    result.list = parallel::mcmapply(lines, seq_along(lines), SIMPLIFY = FALSE, FUN = function(line, line.id) {
+        if ( nchar(line) == 0 ) {
+            return(NULL)
+        }
+
+        # 1) split key
+        # 2) split value
+        line.split = unlist(strsplit(line, SEPERATOR))
+        key = line.split[1]
+        value = line.split[2]
+
+        # Transform data to data.frame
+        df = merge(key, value)
+        colnames(df) = c("author.name", "gender")
+        return(df)
+    })
+
+    result.df = plyr::rbind.fill(result.list)
+    logging::logdebug("read.gender: finished.")
+    return(result.df)
+}
+
+#' Create an empty dataframe which has the same shape as a dataframe containing issues. The dataframe has the column
+#' names and column datatypes defined in \code{ISSUES.LIST.COLUMNS} and \code{ISSUES.LIST.DATA.TYPES}, respectively.
+#'
+#' @return the empty dataframe
+create.empty.issues.list = function() {
+    return (create.empty.data.frame(ISSUES.LIST.COLUMNS, ISSUES.LIST.DATA.TYPES))
+}
+
+#' Create an empty dataframe which has the same shape as a dataframe containing gender data.
+#' The dataframe has the column names and column datatypes defined in \code{GENDER.LIST.COLUMNS}
+#' and \code{GENDER.LIST.DATA.TYPES}, respectively.
+#'
+#' @return the empty dataframe
+create.empty.gender.list = function() {
+    return (create.empty.data.frame(GENDER.LIST.COLUMNS, GENDER.LIST.DATA.TYPES))
+}
+
 
 ## * Commit message data ---------------------------------------------------
 
