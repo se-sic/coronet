7335c3dd4d0302b024a66d18701d9800ed3fe806
Leo Sendelbach <s8lesend@stud.uni-saarland.de>
2024-01-09 16:25:24 +0100

    Add tests for get.expanded.adjacency.cumulated
    Signed-off by: Leo Sendelbach <s8lesend@stud.uni-saarland.de>

--- tests/test-util-networks-misc.R
+++ tests/test-util-networks-misc.R
@@ -631,3 +631,109 @@ test_that("getting a sparse adjacency matrix per network, two networks", {
     ## Assert
     expect_equal(list(matrix.out.one, matrix.out.two), result)
 })
+
+test_that("getting cumulative sums of adjacency matrices generated from networks, two networks", {
+
+    ## Arrange
+    vertices = data.frame(
+        name = c("Heinz", "Dieter", "Klaus"),
+        kind = TYPE.AUTHOR,
+        type = TYPE.AUTHOR
+        )
+    edges = data.frame(
+        from = c("Heinz", "Dieter", "Dieter"),
+        to = c("Dieter", "Klaus", "Heinz")
+        )
+    network.in.one = igraph::graph.data.frame(edges, directed = FALSE, vertices = vertices)
+    authors.in.one = sort(c("Heinz", "Dieter", "Klaus"))
+
+    matrix.out.one = Matrix::sparseMatrix(i = c(), j = c(), x = 0, 
+                                            dims = c(length(authors.in.one), length(authors.in.one)),
+                                            repr = "T")
+    rownames(matrix.out.one) = authors.in.one
+    colnames(matrix.out.one) = authors.in.one
+
+    matrix.out.one["Heinz", "Dieter"] = 1
+    matrix.out.one["Klaus", "Dieter"] = 1
+    matrix.out.one["Dieter", "Heinz"] = 1
+    matrix.out.one["Dieter", "Klaus"] = 1
+
+    edges = data.frame(
+        from = c("Klaus"),
+        to = c("Dieter")
+        )
+    network.in.two = igraph::graph.data.frame(edges, directed = FALSE, vertices = vertices)
+    authors.in.two = sort(c("Heinz", "Dieter", "Klaus"))
+
+    matrix.out.two = Matrix::sparseMatrix(i = c(), j = c(), x = 0, 
+                                            dims = c(length(authors.in.two), length(authors.in.two)),
+                                            repr = "T")
+    rownames(matrix.out.two) = authors.in.two
+    colnames(matrix.out.two) = authors.in.two
+
+    matrix.out.two["Heinz", "Dieter"] = 1
+    matrix.out.two["Klaus", "Dieter"] = 1
+    matrix.out.two["Dieter", "Heinz"] = 1
+    matrix.out.two["Dieter", "Klaus"] = 1
+    
+    ## Act
+    result = get.expanded.adjacency.cumulated(networks = list(network.in.one, network.in.two))
+    
+    ## Assert
+    compare.sparse.matrices(matrix.out.one, result[[1]])
+    compare.sparse.matrices(matrix.out.two, result[[2]])
+})
+
+test_that("getting cumulative sums of adjacency matrices generated from networks, two networks, weighted", {
+
+    ## Arrange
+    vertices = data.frame(
+        name = c("Heinz", "Dieter", "Klaus"),
+        kind = TYPE.AUTHOR,
+        type = TYPE.AUTHOR
+        )
+    edges = data.frame(
+        from = c("Heinz", "Dieter", "Dieter"),
+        to = c("Dieter", "Klaus", "Heinz"),
+        weight = c(1, 2, 1)
+        )
+    network.in.one = igraph::graph.data.frame(edges, directed = FALSE, vertices = vertices)
+    authors.in.one = sort(c("Heinz", "Dieter", "Klaus"))
+
+    matrix.out.one = Matrix::sparseMatrix(i = c(), j = c(), x = 0, 
+                                            dims = c(length(authors.in.one), length(authors.in.one)),
+                                            repr = "T")
+    rownames(matrix.out.one) = authors.in.one
+    colnames(matrix.out.one) = authors.in.one
+
+    matrix.out.one["Heinz", "Dieter"] = 2
+    matrix.out.one["Klaus", "Dieter"] = 2
+    matrix.out.one["Dieter", "Heinz"] = 2
+    matrix.out.one["Dieter", "Klaus"] = 2
+
+    edges = data.frame(
+        from = c("Klaus"),
+        to = c("Dieter"),
+        weight = c(1)
+        )
+    network.in.two = igraph::graph.data.frame(edges, directed = FALSE, vertices = vertices)
+    authors.in.two = sort(c("Heinz", "Dieter", "Klaus"))
+
+    matrix.out.two = Matrix::sparseMatrix(i = c(), j = c(), x = 0, 
+                                            dims = c(length(authors.in.two), length(authors.in.two)),
+                                            repr = "T")
+    rownames(matrix.out.two) = authors.in.two
+    colnames(matrix.out.two) = authors.in.two
+    
+    matrix.out.two["Heinz", "Dieter"] = 2
+    matrix.out.two["Klaus", "Dieter"] = 3
+    matrix.out.two["Dieter", "Heinz"] = 2
+    matrix.out.two["Dieter", "Klaus"] = 3
+    
+    ## Act
+    result = get.expanded.adjacency.cumulated(networks = list(network.in.one, network.in.two), weighted = TRUE)
+    
+    ## Assert
+    compare.sparse.matrices(matrix.out.one, result[[1]])
+    compare.sparse.matrices(matrix.out.two, result[[2]])
+})
