bd3e2c1a9fe1cf9238105bd5f8b808988e42319a
Leo Sendelbach <s8lesend@stud.uni-saarland.de>
2025-03-11 16:09:31 +0100

    Update required Matrix version
    force matrix version 1.5.0 or higher as required for textstem to work
    
    Signed-off-by: Leo Sendelbach <s8lesend@stud.uni-saarland.de>

--- install.R
+++ install.R
@@ -79,11 +79,14 @@ if (length(p) > 0) {
     }
 
     Matrix.version = installed.packages()[rownames(installed.packages()) == "Matrix", "Version"]
-    if (compareVersion(Matrix.version, "1.3.0") == -1) {
-        print("WARNING: Matrix version 1.3.0 or higher is necessary for using coronet. Re-install package Matrix...")
-        install.packages("Matrix", dependencies = NA, verbose = TRUE, quiet = TRUE)
+    if (compareVersion(Matrix.version, "1.5.0") == -1) {
+        print("WARNING: Matrix version 1.5.0 or higher is necessary for using coronet. Re-install package Matrix...")
+        matrix.1.5.4.url = "https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.5-4.tar.gz"
+        install.packages(matrix.1.5.4.url, repos = NULL, dependencies = NA, verbose = TRUE, quiet = TRUE)
+        ## redo installation of textstem, which fails if matrix is outdated or not present
+        install.packages("textstem", dependencies = NA, verbose = TRUE, quiet = TRUE)
         Matrix.version = installed.packages()[rownames(installed.packages()) == "Matrix", "Version"]
-        if (compareVersion(Matrix.version, "1.3.0") == -1) {
+        if (compareVersion(Matrix.version, "1.5.0") == -1) {
             print("WARNING: Re-installation of package Matrix did not end up in the necessary package version.")
         }
     }
